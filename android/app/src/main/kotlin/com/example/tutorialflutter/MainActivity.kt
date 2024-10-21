9b70fa771130b43c20b6feec93257b145579ea15
wjinggoh <wjingggoh15@gmail.com>
2024-10-21 23:17:40 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/tutorialflutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tutorialflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/weather_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:tutorialflutter/models/weather_model.dart';
+import 'package:tutorialflutter/services/weather_service.dart';
+
+class WeatherPage extends StatefulWidget{
+  const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() =>_WeatherPageState();
+
+}
+
+class _WeatherPageState extends State<WeatherPage>{
+
+  //api key
+  final _weatherService =WeatherService('YOUR API KEY');
+  Weather? _weather;
+
+  //fetch weather
+  _fetchWeather() async{
+    //get the current city
+    String cityName= await _weatherService.getCurrentCity();
+
+    //get weather for city
+    try{
+      final weather= await _weatherService.getWeather(cityName);
+      setState((){
+        _weather =weather;
+      });
+    }
+
+    //any errors
+    catch(e){
+      print(e);
+    }
+
+  }
+
+  //weather animations
+
+  //init state
+  @override
+  void initState(){
+    super.initState();
+
+    //fetch weather on startup
+    _fetchWeather();
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body:Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //city name
+          Text(_weather?.cityName ?? "loading city..."),
+
+          //temperature
+          Text('${_weather?.temperature.round()} C')
+        ],
+      )
+    )
+    );
+  }
+}
